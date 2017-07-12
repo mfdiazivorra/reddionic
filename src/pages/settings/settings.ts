f71d3f99e88b5b14755f4fe7e434c55a89a326cb
mfdiazivorra <prozaconsixstrings@gmail.com>
2017-07-12 11:31:35 +0200

    added category & limit selection

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RedditService } from "../../app/services/reddit.service";
+import { RedditsPage } from "../reddits/reddits";
 
 @Component({
   selector: 'settings',
@@ -7,8 +9,32 @@ import { NavController } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {
+  category: any;
+  limit: any;
 
+  constructor(public navCtrl: NavController, private redditService:RedditService) {
+    this.getDefaults();
+  }
+
+
+  getDefaults(){
+      if (localStorage.getItem('category') != null) {
+        this.category = localStorage.getItem('category');
+      } else {
+        this.category = 'sports';
+      }
+
+      if (localStorage.getItem('limit') != null) {
+        this.limit = localStorage.getItem('limit');
+      } else {
+        this.limit = 10;
+      }
+  }
+
+  setDefaults(){
+    localStorage.setItem('category', this.category);
+    localStorage.setItem('limit', this.limit);
+    this.navCtrl.push(RedditsPage);
   }
 
 }
